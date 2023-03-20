668b3c65155bbfa1aa731d3b2304db53871802cb
Roger Moraes <roger.moura@ufrgs.br>
2023-03-19 21:34:39 -0300

    Terminando carrossel de imagens

---
+++ Mobile-Fist/Vendors/js/swiper.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    slidesPerview:3,
+    spaceBetween: 10,
+    autoplay:{
+        delay:500,
+    },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+
+  });
+
